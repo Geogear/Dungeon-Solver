87fbee2decc209d2319218a67e9b3357e05f63f2
unknown <mustafaenesbilgic@hotmail.com>
2021-10-25 21:42:05 +0300

    Add enums for dungeon names.

---
+++ Assets/Scripts/DungeonNames.cs
@@ -0,0 +1,15 @@
+public enum Direction
+{
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+    DirectionCount = 4
+}
+
+public enum Tile
+{
+    Normal = 0,
+    DoorTile = 1,
+    TileCount = 2
+}
