a5e04c2c9785c4497c753f1e8cb7499b1a6b5bd6
unknown <mustafaenesbilgic@hotmail.com>
2021-12-01 13:35:52 +0300

    Add puzzle displayer to display different puzzles.

---
+++ Assets/Scripts/PuzzleDisplayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDisplayer : MonoBehaviour
+{
+    /* TODO instantiated as a child of the player. 
+       + have gameobject fields for ctp tiles. */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
