e1319932c37c3c90ebc74134cde5331de8537619
unknown <mustafaenesbilgic@hotmail.com>
2022-01-15 13:39:24 +0300

    Update spell animations.

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -6,6 +6,7 @@ public class Boss : MonoBehaviour
 
     private const float FaceTurningCap = 0.5f;
 
+    [SerializeField] private string _bossName = "";
     [SerializeField] private float _attackDamage = 5.0f;
     [SerializeField] private float _attackRange = 0.35f;
     [SerializeField] private float _attackRate = 1.0f;
@@ -16,12 +17,15 @@ public class Boss : MonoBehaviour
     [SerializeField] private int _hitPoints = 15;
     [SerializeField] private LayerMask _targetLayer;
     [SerializeField] private Transform _attackLocation;
+    [SerializeField] private Animator _spellAnimator = null;
 
     private SpriteRenderer _spriteRenderer = null;
     private BoxCollider2D _boxCollider2D = null;
     private Animator _animator = null;
     private float _nextAttackTime = 0.0f;
     private float _nextSpellTime = 0.0f;
+    private float _spellCastPlayTime = 0.0f;
+    private float _spellLeftCD = 0.0f;
     private bool _contAttack = false;
     private bool _contSpellAttack = false;
     private bool _facingRight = true;
@@ -31,16 +35,27 @@ public class Boss : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _boxCollider2D = GetComponent<BoxCollider2D>();
         _animator = GetComponent<Animator>();
+        _spellCastPlayTime = GetAnimFromAnimator(_spellAnimator, _bossName + "Spell");
     }
 
     // Update is called once per frame
     void Update()
     {
+        /* If out of range, stop spell.*/
+        if(_contSpellAttack &&
+            Vector3.Distance(_playerTransform.position, transform.position) > _spellRange)
+        {
+            _contSpellAttack = false;
+        }
+
         /* If player in spell attack range start spell attack. */
         if(!_contSpellAttack && !_contAttack &&
             Vector3.Distance(_playerTransform.position, transform.position) <= _spellRange)
         {
             _contSpellAttack = true;
+            _spellLeftCD = _spellCastPlayTime;
+            _nextSpellTime = -1f;
+            _animator.SetTrigger("Spell");
         }
 
         /* If player in normal attack range start normal attack. */
@@ -63,10 +78,29 @@ public class Boss : MonoBehaviour
             }
         }
 
-        if(_contSpellAttack && Time.time >= _nextSpellTime)
+        if(_contSpellAttack)
         {
-            /* TODO, spell attack. */
-            _nextSpellTime = Time.time + 1f / _spellRate;
+            /* Casting the spell. */
+            if(_spellLeftCD > float.Epsilon + _spellCastPlayTime * 0.1f)
+            {
+                _spellLeftCD -= Time.deltaTime;
+            }
+            else
+            {
+                /* Casting is done either waiting for the next cast,
+                 * or trigger the spell object and start waiting. */
+                if(_nextSpellTime == -1f)
+                {
+                    /* TODO trigger spell object. */
+                    _nextSpellTime = Time.time + 1f / _spellRate;
+                }
+                else if(Time.time > _nextSpellTime)
+                {
+                    _nextSpellTime = -1f;
+                    _spellLeftCD = _spellCastPlayTime;
+                    _animator.SetTrigger("Spell");
+                }
+            }
         }
     }
 
@@ -97,6 +131,18 @@ public class Boss : MonoBehaviour
         {
             _contSpellAttack = _contSpellAttack = false;
         }
+    }
+
+    private float GetAnimFromAnimator(Animator animator, string name)
+    {
+        for (int i = 0; i < animator.runtimeAnimatorController.animationClips.Length; i++)
+        {
+            if (animator.runtimeAnimatorController.animationClips[i].name == name)
+            {
+                return animator.runtimeAnimatorController.animationClips[i].length;
+            }
+        }
+        return 0.0f;
     }
 
     public void GetHit(float damage)
