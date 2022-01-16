454084cbd0c3bcacd293ad8733e6d0dc92376a15
unknown <mustafaenesbilgic@hotmail.com>
2022-01-16 19:06:39 +0300

    Add 2 more playable characters.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private GameObject _characterSelection;
+    private GameObject _mainMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _characterSelection = GameObject.FindGameObjectWithTag("CharacterSelection");
+        _characterSelection.SetActive(false);
+        _mainMenu = GameObject.FindGameObjectWithTag("PauseMenu");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void MMMOnFakePlay()
+    {
+        _characterSelection.SetActive(true);
+        _mainMenu.SetActive(false);
     }
 
     public void MMMOnPlay()
@@ -22,5 +32,15 @@ public class MainMenuManager : MonoBehaviour
     public void MMMOnQuit()
     {
         Application.Quit();
+    }
+
+    public void CharacterSelect1() { CharacterSelect(FAType.FallenAngel1); }
+    public void CharacterSelect2() { CharacterSelect(FAType.FallenAngel2); }
+    public void CharacterSelect3() { CharacterSelect(FAType.FallenAngel3); }
+
+    private void CharacterSelect(FAType selection)
+    {
+        PlayerCharacter._FAType = selection;
+        MMMOnPlay();
     }
 }
